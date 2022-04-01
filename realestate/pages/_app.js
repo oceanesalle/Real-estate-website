7ba852ecbf2da5f618484bcce9a00c0b26c803e4
oceansalle <oceanesalleiae@gmail.com>
2022-04-01 14:42:16 +0200

    property2

--- realestate/pages/_app.js
+++ realestate/pages/_app.js
@@ -1,7 +1,11 @@
-import '../styles/globals.css'
+import Router from 'next/router';
+import Head from 'next/head';
+import NProgress from 'nprogress';
+import { ChakraProvider } from '@chakra-ui/react';
+
+import Layout from '../components/Layout';
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
-}
+  NProgress.configure({ showSpinner: false });
 
 export default MyApp
