2fd652a2856e04e0a0293ce6712de3dad038ec00
oceansalle <oceanesalleiae@gmail.com>
2022-04-04 15:34:09 +0200

    property details

--- realestate/components/Property.jsx
+++ realestate/components/Property.jsx
@@ -6,6 +6,7 @@ import { FaBed, FaBath } from 'react-icons/fa';
 import { BsGridFill } from 'react-icons/bs';
 import { GoVerified } from 'react-icons/go';
 import millify from 'millify';
+
 import DefaultImage from '../assets/images/house.jpg';
 
 const Property = ({ property: { coverPhoto, price, rentFrequency, rooms, title, baths, area, agency, isVerified, externalID  } }) => (
@@ -34,7 +35,6 @@ const Property = ({ property: { coverPhoto, price, rentFrequency, rooms, title, 
       </Box>
     </Flex>
   </Link>
-
-); 
+);
 
 export default Property;
